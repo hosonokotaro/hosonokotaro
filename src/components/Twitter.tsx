9ea2ca38f84da39191a450bbf9243c8b2b22d3d4
hosono <hosono1985@gmail.com>
2020-05-03 11:13:12 +0900

    wip split twitter component

---
+++ src/components/Twitter.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import {
+  Section,
+  SectionTitle,
+} from '../components/Section';
+
+const Twitter: React.FC = () => {
+  useEffect(() => {
+    const scriptTag = document.createElement('script');
+    scriptTag.src = 'https://platform.twitter.com/widgets.js';
+    scriptTag.charset = 'utf-8';
+
+    document.getElementById('twitter')?.appendChild(scriptTag);
+  });
+
+  return (
+    <Section id="twitter">
+      <SectionTitle>
+        twitter
+      </SectionTitle>
+      <BoxTwitter>
+        <a
+          className="twitter-timeline"
+          data-lang="ja"
+          data-height="667"
+          data-theme="light"
+          data-tweet-limit="5"
+          href="https://twitter.com/hosono_fe?ref_src=twsrc%5Etfw"
+        >
+          Tweets by hosono_fe
+        </a>
+      </BoxTwitter>
+      <BoxTwitterButton>
+        <a
+          href="https://twitter.com/hosono_fe?ref_src=twsrc%5Etfw"
+          className="twitter-follow-button"
+          data-lang="ja"
+          data-show-count="false"
+        >
+          Follow @hosono_fe
+        </a>
+      </BoxTwitterButton>
+    </Section>
+  );
+};
+
+const BoxTwitter = styled.div`
+  margin-top: 2em;
+  padding: 5px;
+  background: #f1f1f1;
+  font-size: 1.4rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+const BoxTwitterButton = styled.div`
+  padding-top: 2em;
+  line-height: 1;
+`;
+
+export default Twitter;
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import Twitter from '../components/Twitter';
 import {
   Section,
   SectionTitle,
@@ -10,17 +11,9 @@ import txtKitakuTtl from '../images/txt_kitaku_ttl.png';
 import picKitakuBg from '../images/pic_kitaku_bg.jpg';
 
 const Top: React.FC = () => {
-  useEffect(() => {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = 'https://platform.twitter.com/widgets.js';
-    scriptTag.charset = 'utf-8';
-
-    document.getElementById('twitter')?.appendChild(scriptTag);
-  });
-
   return (
     <>
-      <Bg>
+      <FirstView>
         <figure>
           <img src={picKitaku} alt="北区" />
         </figure>
@@ -32,7 +25,7 @@ const Top: React.FC = () => {
           戦後の労働者たちは夜勤明けに、赤羽の飲み屋街に集まり酒を飲んだという。<br />
           私は、そんな粗野な印象を残す街で、親子三世代に渡って生活をしている。
         </TxtCaption>
-      </Bg>
+      </FirstView>
       <Section>
         <SectionTitle>
           Speak
@@ -49,38 +42,12 @@ const Top: React.FC = () => {
           </li>
         </SectionList>
       </Section>
-      <Section id="twitter">
-        <SectionTitle>
-          twitter
-        </SectionTitle>
-        <BoxTwitter>
-          <a
-            className="twitter-timeline"
-            data-lang="ja"
-            data-height="667"
-            data-theme="light"
-            data-tweet-limit="5"
-            href="https://twitter.com/hosono_fe?ref_src=twsrc%5Etfw"
-          >
-            Tweets by hosono_fe
-          </a>
-        </BoxTwitter>
-        <BoxTwitterButton>
-          <a
-            href="https://twitter.com/hosono_fe?ref_src=twsrc%5Etfw"
-            className="twitter-follow-button"
-            data-lang="ja"
-            data-show-count="false"
-          >
-            Follow @hosono_fe
-          </a>
-        </BoxTwitterButton>
-      </Section>
+      <Twitter />
     </>
   );
 };
 
-const Bg = styled.div`
+const FirstView = styled.section`
   margin: 0 auto;
   padding: 4em;
   text-align: center;
@@ -118,21 +85,6 @@ const IconText = styled.span`
   padding: 0 5px;
   background: #000;
   color: #fff;
-`;
-
-const BoxTwitter = styled.div`
-  margin-top: 2em;
-  padding: 5px;
-  background: #f1f1f1;
-  font-size: 1.4rem;
-
-  @media (min-width: 768px) {
-    font-size: 1.8rem;
-  }
-`;
-
-const BoxTwitterButton = styled.div`
-  padding-top: 2em;
 `;
 
 export default Top;
