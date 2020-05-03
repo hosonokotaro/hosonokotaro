416ca5050495275ae2da0085aac99f7baac1c02d
hosono <hosono1985@gmail.com>
2020-05-04 00:24:42 +0900

    add pages

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import { siteTitle } from '../components/SiteSetting';
+import {
+  Section,
+  SectionTitle,
+} from '../components/Section';
+
+import picHosono from '../images/pic_hosono.jpg';
+import picMail from '../images/pic_mail.png';
+
+const Contact: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>CONTACT | {siteTitle}</title>
+      </Helmet>
+      <Section>
+        <SectionTitle>
+          Contact
+        </SectionTitle>
+        <Figure>
+          <img src={picHosono} alt="細野" />
+        </Figure>
+        <TitleName>
+          HOSONO KOTARO
+        </TitleName>
+        <TextDetail>
+          <ruby>細野 広太郎<rt>ほその こうたろう</rt></ruby><br />
+          1985年生まれ<br />
+          <br />
+          東京でフロントエンドエンジニアとして、ミレニアル世代のフリーランスとして活動している。<br />
+          <br />
+          大手企業などのサイトを受託案件として多数手がける。個人としては、アニメーション制作会社、個人経営の本屋さんのサイトも手がける。<br />
+          <br />
+          好きな言葉は「<ruby>色即是空<rt>しきそくぜくう</rt></ruby>」<br />
+          <br />
+          <a href="mailto:hosono1985@gmail.com">
+            <IcomMail src={picMail} alt="Mail" />
+          </a>
+        </TextDetail>
+      </Section>
+    </>
+  );
+}
+
+const Figure = styled.figure`
+  position: relative;
+  width: 180px;
+  margin: 0 auto;
+  padding-top: 2em;
+
+  @media (min-width: 768px) {
+    width: 360px;
+  }
+
+  img {
+    width: 100%;
+    height: 180px;
+    border-radius: 50%;
+    overflow: hidden;
+    object-fit: cover;
+
+    @media (min-width: 768px) {
+      height: 360px;
+    }
+  }
+`;
+
+const TitleName = styled.h3`
+  padding-top: 0.8em;
+  line-height: 1;
+  font-size: 1.8rem;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 2.4rem;
+  }
+`;
+
+const TextDetail = styled.p`
+  padding-top: 2em;
+  font-size: 1.4rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+const IcomMail = styled.img`
+  width: 25px;
+  
+  @media (min-width: 768px) {
+    width: auto;
+  }
+`;
+
+export default Contact;
---
+++ src/pages/Top.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import { siteTitle } from '../components/SiteSetting';
+import Twitter from '../components/Twitter';
+import {
+  Section,
+  SectionTitle,
+} from '../components/Section';
+
+import picKitaku from '../images/pic_kitaku.png';
+import txtKitakuTtl from '../images/txt_kitaku_ttl.png';
+import picKitakuBg from '../images/pic_kitaku_bg.jpg';
+
+const Top: React.FC = () => {
+  return (
+    <>
+      <Helmet>
+        <title>TOP | {siteTitle}</title>
+      </Helmet>
+      <FirstView>
+        <figure>
+          <img src={picKitaku} alt="北区" />
+        </figure>
+        <Title>
+          <img src={txtKitakuTtl} alt="東京都北区赤羽" />
+        </Title>
+        <TxtCaption>
+          東京都北区赤羽、北の玄関口。<br />
+          戦後の労働者たちは夜勤明けに、赤羽の飲み屋街に集まり酒を飲んだという。<br />
+          私は、そんな粗野な印象を残す街で、親子三世代に渡って生活をしている。
+        </TxtCaption>
+      </FirstView>
+      <Section>
+        <SectionTitle>
+          Speak
+        </SectionTitle>
+        <SectionList>
+          <li>
+            <IconText>GitHub</IconText><a href="https://github.com/hosonokotaro/" target="_blank" rel="noopener noreferrer">https://github.com/hosonokotaro/</a>
+          </li>
+          <li>
+            <IconText>Qiita</IconText><a href="https://qiita.com/hosono" target="_blank" rel="noopener noreferrer">https://qiita.com/hosono</a>
+          </li>
+          <li>
+            <IconText>note</IconText><a href="https://note.mu/hosonokotaro" target="_blank" rel="noopener noreferrer">https://note.mu/hosonokotaro</a>
+          </li>
+        </SectionList>
+      </Section>
+      <Twitter />
+    </>
+  );
+};
+
+const FirstView = styled.section`
+  margin: 0 auto;
+  padding: 4em;
+  text-align: center;
+  background: url(${picKitakuBg}) top 30% center no-repeat;
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  padding-top: 2em;
+`;
+
+const TxtCaption = styled.p`
+  padding-top: 2em;
+  line-height: 1.8;
+  font-size: 1.4rem;
+  color: #ddd;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+const SectionList = styled.ul`
+  padding-top: 2em;
+  line-height: 1.8;
+  font-size: 1.4rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+const IconText = styled.span`
+  margin-right: 5px;
+  padding: 0 5px;
+  background: #000;
+  color: #fff;
+`;
+
+export default Top;
