ec2a8d946b7c8730d6ef74b58cf858689ebbc033
hosonokotaro <hosono1985@gmail.com>
2020-10-08 13:12:12 +0900

    update packages

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import Helmet from 'react-helmet';
 import styled from 'styled-components/macro';
 import { siteTitle } from '../SiteSetting';
 import Section from '../components/Section';
